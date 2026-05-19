2118434d179a34c6d53f08c52be2fad4c2f04894
Artur-MacBook-Air <artur@example.com>
2026-05-19 19:10:10 +0300

    Route system started step

---
+++ database/migrations/2026_05_19_160213_create_routes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignIdColumnDefinition;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('routes', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+
+            $table->string('start_address');
+            $table->string('end_address');
+
+            $table->integer('distance_km');
+
+            $table->text('comment')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('routes');
+    }
+};
