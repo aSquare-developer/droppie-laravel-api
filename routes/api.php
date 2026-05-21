dca7f6ad76d49c46a4bbc591fd20e8e61d881e69
Artur-MacBook-Air <artur@example.com>
2026-05-21 18:39:55 +0300

    Create first API endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\RouteController;
 
 Route::get('/hello', function () {
     return response()->json([
         'message' => 'Hello DroppieTrack'
     ]);
 });
+
+Route::apiResource('routes', RouteController::class);
