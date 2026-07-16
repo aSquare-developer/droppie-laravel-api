3e0f7e9cf7cab3aff7425caf1cc5d64e3c6ba73b
Artur-MacBook-Air <artur@example.com>
2026-07-16 21:01:21 +0300

    Fix Vite manifest lookup in feature tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -15,7 +15,10 @@ use Tests\TestCase;
 */
 
 pest()->extend(TestCase::class)
- // ->use(RefreshDatabase::class)
+    ->beforeEach(function (): void {
+        $this->withoutVite();
+    })
+    // ->use(RefreshDatabase::class)
     ->in('Feature');
 
 /*
